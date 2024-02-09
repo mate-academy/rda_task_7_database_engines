5322a869722ecc2d2ee3c9b846079cbac970c856
AlexHCom <alexhcomgrit@gmail.com>
2024-02-09 22:25:11 +0200

    Solution

--- task.sql
+++ task.sql
@@ -14,26 +14,25 @@ CREATE TABLE GeoIP (
     IPRagne VARCHAR(50),
     CountryID INT,
     PRIMARY KEY (ID)
-) ENGINE=MEMORY;
+) ENGINE=Memory;
 -- Create a table for storing product descriptions for different countries (Columns: ID, CountryID, ProductID, Description )
 CREATE TABLE ProductDescription(
     ID INT,
     CountryID INT,
     ProductID INT,
-    Description VARCHAR(100),
-    PRIMARY KEY (ID),
-
-) ENGINE=InnoDB;
+    Description VARCHAR(500),
+    PRIMARY KEY (ID)
+) ENGINE=MyISAM;
 -- Create a table for storing logs. For now we don't need to save them, but we need to implement functionality (Columns: ID, Time, LogRecord)
-CREATE TABLE Logs(
+CREATE TABLE LogsTable(
     ID INT,
     Time TIMESTAMP,
     LogRecord VARCHAR(200),
     PRIMARY KEY (ID)
 ) ENGINE=BLACKHOLE;
 -- Create a table for storing reporting data, which will be send to a separate application in the CSV format for analytics purposes (Columns:  Date, ProductName, Orders)
-CREATE TABLE ProductReport(
+CREATE TABLE ProductReporting(
     Date DATE,
-    ProductName VARCHAR(100),
-    Orders INT
+    ProductName VARCHAR(100) NOT NULL,
+    Orders INT NOT NULL
 ) ENGINE=CSV;
