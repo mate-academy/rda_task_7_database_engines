7e6731cbe756295a2642140379f843118bf18eb7
Volodymyr Panov <vpanov25@gmail.com>
2024-04-04 16:55:08 +0200

    Solution4

--- task.sql
+++ task.sql
@@ -35,7 +35,7 @@ CREATE TABLE LogsTable (
 
 -- Create a table for storing reporting data, which will be send to a separate application in the CSV format for analytics purposes (Columns:  Date, ProductName, Orders)
 CREATE TABLE ProductReporting (
-    Date DATE,
-    ProductName VARCHAR(70),
-    Orders INT
+    Date DATE NOT NULL,
+    ProductName VARCHAR(70) NOT NULL,
+    Orders INT NOT NULL
 ) ENGINE=CSV;
