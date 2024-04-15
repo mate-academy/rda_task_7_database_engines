85c25bd1a9686cb7aab10cbba59a62ae07bb43a5
Marian Kostiuk <131563162+kostiukmkalne@users.noreply.github.com>
2024-04-14 22:52:30 -0500

    solution

--- task.sql
+++ task.sql
@@ -13,9 +13,9 @@ CREATE TABLE GeoIPCache (
     ID INT,
     IPRange VARCHAR(50),
     CountryID INT,
-    FOREIGN KEY (CountryID) REFERENCES Countries(ID) ON DELETE SET NULL,
+    FOREIGN KEY (CountryID) REFERENCES Countries(ID) ON DELETE CASCADE,
     PRIMARY KEY (ID)
-) ENGINE=MEMORY;
+) ENGINE=Memory;
 
 -- Create a table for storing product descriptions for different countries (Columns: ID, CountryID, ProductID, Description )
 CREATE TABLE ProductDescription (
@@ -23,21 +23,21 @@ CREATE TABLE ProductDescription (
     Description VARCHAR(100),
     ProductID INT,
     CountryID INT,
-    FOREIGN KEY (CountryID) REFERENCES Countries(ID) ON DELETE SET NULL,
+    FOREIGN KEY (CountryID) REFERENCES Countries(ID) ON DELETE CASCADE,
     PRIMARY KEY (ID)
-) ENGINE=InnoDB;
+) ENGINE=MyISAM;
 
 -- Create a table for storing logs. For now we don't need to save them, but we need to implement functionality (Columns: ID, Time, LogRecord)
-CREATE TABLE Logs (
+CREATE TABLE LogsTable (
     ID INT,
     Timestamp DATE,
     Message VARCHAR(100),
     PRIMARY KEY (ID)
-) ENGINE=BLACKHOLE;
+) ENGINE=Blackhole;
 
 -- Create a table for storing reporting data, which will be sent to a separate application in the CSV format for analytics purposes (Columns:  Date, ProductName, Orders)
 CREATE TABLE ProductReporting (
     Date DATE NOT NULL,
     ProductName VARCHAR(50) NOT NULL,
-    Orders INT NOT NULL
+    Orders VARCHAR(100) NOT NULL
 ) ENGINE=CSV;
