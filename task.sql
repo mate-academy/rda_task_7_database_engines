23968e28f4bdeb4df0c822b9eb426a5f162449a7
SHCHERBANV <shcherbanvasyl@gmail.com>
2024-04-24 19:13:05 -0600

    Solution

--- task.sql
+++ task.sql
@@ -1,38 +1,38 @@
-CREATE DATABASE IF NOT EXISTS ShopDB; 
+CREATE DATABASE ShopDB; 
 USE ShopDB; 
 
-CREATE TABLE IF NOT EXISTS Countries (
+CREATE TABLE Countries (
     ID INT,
     Name VARCHAR(50),
     PRIMARY KEY (ID)
 ) ENGINE=InnoDB;
 
-CREATE TABLE IF NOT EXISTS GeoIPCache (
-    ID INT AUTO_INCREMENT,
+CREATE TABLE GeoIPCache (
+    ID INT,
     IPRange VARCHAR(50),
     CountryID INT,
+    FOREIGN KEY (CountryID) REFERENCES Countries(ID) ON DELETE CASCADE,
     PRIMARY KEY (ID)
-) ENGINE=MEMORY;
+) ENGINE=Memory;
 
-CREATE TABLE IF NOT EXISTS ProductDescription (
-    ID INT AUTO_INCREMENT,
-    Description TEXT,
+CREATE TABLE ProductDescription (
+    ID INT,
+    Description VARCHAR(100),
     ProductID INT,
     CountryID INT,
-    PRIMARY KEY (ID),
-    INDEX (ProductID),
-    INDEX (CountryID)
-) ENGINE=InnoDB;
-
-CREATE TABLE IF NOT EXISTS Logs (
-    ID INT AUTO_INCREMENT,
-    Timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    Message TEXT,
+    FOREIGN KEY (CountryID) REFERENCES Countries(ID) ON DELETE CASCADE,
     PRIMARY KEY (ID)
-) ENGINE=BLACKHOLE;
+) ENGINE=MyISAM;
 
-CREATE TABLE IF NOT EXISTS ProductReporting (
+CREATE TABLE Logs (
+    ID INT,
+    Timestamp DATE,
+    Message VARCHAR(100),
+    PRIMARY KEY (ID)
+) ENGINE=Blackhole;
+
+CREATE TABLE ProductReporting (
     Date DATE NOT NULL,
-    ProductName VARCHAR(100) NOT NULL,
-    Orders INT NOT NULL
+    ProductName VARCHAR(50) NOT NULL,
+    Orders VARCHAR(100) NOT NULL
 ) ENGINE=CSV;
