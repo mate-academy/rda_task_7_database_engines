2b6e4e27db2c8215d334dca2be728a6241471244
SHCHERBANV <shcherbanvasyl@gmail.com>
2024-04-24 18:55:10 -0600

    Solution

--- task.sql
+++ task.sql
@@ -1,42 +1,38 @@
-CREATE DATABASE ShopDB; 
+CREATE DATABASE IF NOT EXISTS ShopDB; 
 USE ShopDB; 
 
--- Create a table to store countries 
-CREATE TABLE Countries (
+CREATE TABLE IF NOT EXISTS Countries (
     ID INT,
     Name VARCHAR(50),
     PRIMARY KEY (ID)
 ) ENGINE=InnoDB;
 
--- Create a table for caching GeoIP data (Columns: ID, IP Range, CountryID)
-CREATE TABLE GeoIPCache (
-    ID INT,
+CREATE TABLE IF NOT EXISTS GeoIPCache (
+    ID INT AUTO_INCREMENT,
     IPRange VARCHAR(50),
     CountryID INT,
     PRIMARY KEY (ID)
 ) ENGINE=MEMORY;
 
--- Create a table for storing product descriptions for different countries (Columns: ID, CountryID, ProductID, Description )
-CREATE TABLE ProductDescription (
-    ID INT,
+CREATE TABLE IF NOT EXISTS ProductDescription (
+    ID INT AUTO_INCREMENT,
     Description TEXT,
     ProductID INT,
     CountryID INT,
-    PRIMARY KEY (ID)
+    PRIMARY KEY (ID),
+    INDEX (ProductID),
+    INDEX (CountryID)
 ) ENGINE=InnoDB;
 
--- Create a table for storing logs. For now we don't need to save them, but we need to implement functionality (Columns: ID, Time, LogRecord)
-CREATE TABLE Logs (
-    ID INT,
-    Timestamp DATETIME,
+CREATE TABLE IF NOT EXISTS Logs (
+    ID INT AUTO_INCREMENT,
+    Timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     Message TEXT,
     PRIMARY KEY (ID)
 ) ENGINE=BLACKHOLE;
 
--- Create a table for storing reporting data, which will be send to a separate application in the CSV format for analytics purposes (Columns:  Date, ProductName, Orders)
-CREATE TABLE ProductReporting (
+CREATE TABLE IF NOT EXISTS ProductReporting (
     Date DATE NOT NULL,
     ProductName VARCHAR(100) NOT NULL,
     Orders INT NOT NULL
 ) ENGINE=CSV;
-SHOW TABLES
