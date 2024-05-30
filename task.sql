e053f45a7ae20795bec1aa8d5a9c77907d3c1861
OKochubeii <155968394+OKochubeii@users.noreply.github.com>
2024-05-30 22:02:01 +0300

    SOLUTION NOT NULL!

--- task.sql
+++ task.sql
@@ -37,7 +37,7 @@ CREATE TABLE LogsTable (
 
 -- Create a table for storing reporting data, which will be send to a separate application in the CSV format for analytics purposes (Columns:  Date, ProductName, Orders)
 CREATE TABLE ProductReporting (
-    Date DATE,
-    ProductName VARCHAR(50),
-    Orders VARCHAR(100)
+    Date DATE NOT NULL,
+    ProductName VARCHAR(50) NOT NULL,
+    Orders VARCHAR(100) NOT NULL
 ) ENGINE=CSV;
