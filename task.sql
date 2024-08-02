c06762f491b5cc77f8f8d3b7266fbeba2ca21576
volry <v.v.ryzhuk@gmail.com>
2024-08-02 19:42:56 +0300

    fixed table name -error in test :)

--- task.sql
+++ task.sql
@@ -19,12 +19,23 @@ CREATE TABLE GeoIPCache (
 -- Create a table for storing product descriptions for different countries (Columns: ID, CountryID, ProductID, Description )
 CREATE TABLE ProductDescription (
     ID INT AUTO_INCREMENT,
-    Description TEXT,
-    ProductID INT,
-    CountryID INT,
+    Description TEXT NOT NULL,
+    ProductID INT NOT NULL,
+    CountryID INT NOT NULL,
     PRIMARY KEY (ID)
-) ENGINE=InnoDB;
+) ENGINE=MyISAM;
 
 -- Create a table for storing logs. For now we don't need to save them, but we need to implement functionality (Columns: ID, Time, LogRecord)
+CREATE TABLE LogsTable (
+    ID INT AUTO_INCREMENT,
+    Timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    Message TEXT,
+    PRIMARY KEY (ID)
+) ENGINE=BLACKHOLE;
 
 -- Create a table for storing reporting data, which will be send to a separate application in the CSV format for analytics purposes (Columns:  Date, ProductName, Orders)
+CREATE TABLE ProductReporting (
+    Date DATE NOT NULL,
+    ProductName VARCHAR(100) NOT NULL,
+    Orders INT NOT NULL
+) ENGINE=CSV;
