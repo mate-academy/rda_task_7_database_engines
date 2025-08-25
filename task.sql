6dc83a65a3c637e4601ae43d590b949d078a62f4
Alexander Kisliy <108825919+SashaKisliy@users.noreply.github.com>
2025-08-25 18:12:55 +0300

    fix engine

--- task.sql
+++ task.sql
@@ -1,7 +1,8 @@
-CREATE DATABASE ShopDB; 
+CREATE DATABASE IF NOT EXISTS ShopDB; 
 USE ShopDB; 
 
 -- Create a table to store countries 
+DROP TABLE IF EXISTS Countries;
 CREATE TABLE Countries (
     ID INT,
     Name VARCHAR(50),
@@ -9,6 +10,7 @@ CREATE TABLE Countries (
 ) ENGINE=InnoDB;
 
 -- Create a table for caching GeoIP data (Columns: ID, IP Range, CountryID)
+DROP TABLE IF EXISTS GeoIPCache;
 CREATE TABLE GeoIPCache (
     ID INT,
     IPRange VARCHAR(50),
@@ -17,15 +19,17 @@ CREATE TABLE GeoIPCache (
 ) ENGINE=MEMORY;
 
 -- Create a table for storing product descriptions for different countries (Columns: ID, CountryID, ProductID, Description )
+DROP TABLE IF EXISTS ProductDescription;
 CREATE TABLE ProductDescription (
     ID INT,
     Description TEXT,
     ProductID INT,
     CountryID INT,
     PRIMARY KEY (ID)
-) ENGINE=MyISAM;
+) ENGINE=InnoDB;
 
 -- Create a table for storing logs. For now we don't need to save them, but we need to implement functionality (Columns: ID, Time, LogRecord)
+DROP TABLE IF EXISTS Logs;
 CREATE TABLE Logs (
     ID INT,
     Timestamp TIMESTAMP,
@@ -34,6 +38,7 @@ CREATE TABLE Logs (
 ) ENGINE=BLACKHOLE;
 
 -- Create a table for storing reporting data, which will be send to a separate application in the CSV format for analytics purposes (Columns:  Date, ProductName, Orders)
+DROP TABLE IF EXISTS ProductReporting;
 CREATE TABLE ProductReporting (
     Date DATE NOT NULL,
     ProductName VARCHAR(100) NOT NULL,
