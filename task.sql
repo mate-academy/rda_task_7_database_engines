89b5a4eddba7abfad45a0626541ec3febbe2f9b4
Oleg <oleggavelya@gmail.com>
2026-01-09 16:55:24 +0200

    Solution

--- task.sql
+++ task.sql
@@ -1,7 +1,7 @@
-CREATE DATABASE ShopDB; 
-USE ShopDB; 
+CREATE DATABASE ShopDB;
+USE ShopDB;
 
--- Create a table to store countries 
+-- Create a table to store countries
 CREATE TABLE Countries (
     ID INT,
     Name VARCHAR(50),
@@ -9,9 +9,37 @@ CREATE TABLE Countries (
 ) ENGINE=InnoDB;
 
 -- Create a table for caching GeoIP data (Columns: ID, IP Range, CountryID)
-
--- Create a table for storing product descriptions for different countries (Columns: ID, CountryID, ProductID, Description )
-
--- Create a table for storing logs. For now we don't need to save them, but we need to implement functionality (Columns: ID, Time, LogRecord)
+CREATE TABLE GeoIPCache(
+    ID INT AUTO_INCREMENT,
+    IPRange VARCHAR(50),
+    CountryID INT,
+    FOREIGN KEY (CountryID) REFERENCES Countries(ID),
+    PRIMARY KEY (ID)
+) ENGINE=Memory;
 
 -- Create a table for storing reporting data, which will be send to a separate application in the CSV format for analytics purposes (Columns:  Date, ProductName, Orders)
+CREATE TABLE ProductReporting(
+    ID INT AUTO_INCREMENT,
+    Date DATE,
+    ProductName VARCHAR(50),
+    Orders INT,
+    PRIMARY KEY (ID)
+) ENGINE=InnoDB;
+
+-- Create a table for storing product descriptions for different countries (Columns: ID, CountryID, ProductID, Description )
+CREATE TABLE ProductDescription(
+    ID INT AUTO_INCREMENT,
+    Description VARCHAR(50),
+    ProductID INT,
+    CountryID INT,
+    FOREIGN KEY (CountryID) REFERENCES Countries (ID),
+    FOREIGN KEY (ProductID) REFERENCES ProductReporting (ID),
+    PRIMARY KEY (ID)
+) ENGINE=InnoDB;
+-- Create a table for storing logs. For now we don't need to save them, but we need to implement functionality (Columns: ID, Time, LogRecord)
+CREATE TABLE Logs(
+    ID INT AUTO_INCREMENT,
+    Timestamp TIMESTAMP,
+    Message varchar(50),
+    PRIMARY KEY (ID)
+) ENGINE=Blackhole;
