eeb30bc3827c180856e5bfb7407c0fb4e0cb0352
Oleg <oleggavelya@gmail.com>
2026-01-09 17:07:43 +0200

    change engine ProductReporting and ProductDescription and del foreign key ProductID

--- task.sql
+++ task.sql
@@ -17,15 +17,6 @@ CREATE TABLE GeoIPCache(
     PRIMARY KEY (ID)
 ) ENGINE=Memory;
 
--- Create a table for storing reporting data, which will be send to a separate application in the CSV format for analytics purposes (Columns:  Date, ProductName, Orders)
-CREATE TABLE ProductReporting(
-    ID INT AUTO_INCREMENT,
-    Date DATE,
-    ProductName VARCHAR(50),
-    Orders INT,
-    PRIMARY KEY (ID)
-) ENGINE=InnoDB;
-
 -- Create a table for storing product descriptions for different countries (Columns: ID, CountryID, ProductID, Description )
 CREATE TABLE ProductDescription(
     ID INT AUTO_INCREMENT,
@@ -33,9 +24,9 @@ CREATE TABLE ProductDescription(
     ProductID INT,
     CountryID INT,
     FOREIGN KEY (CountryID) REFERENCES Countries (ID),
-    FOREIGN KEY (ProductID) REFERENCES ProductReporting (ID),
     PRIMARY KEY (ID)
-) ENGINE=InnoDB;
+) ENGINE=MyISAM;
+
 -- Create a table for storing logs. For now we don't need to save them, but we need to implement functionality (Columns: ID, Time, LogRecord)
 CREATE TABLE Logs(
     ID INT AUTO_INCREMENT,
@@ -43,3 +34,11 @@ CREATE TABLE Logs(
     Message varchar(50),
     PRIMARY KEY (ID)
 ) ENGINE=Blackhole;
+
+-- Create a table for storing reporting data, which will be send to a separate application in the CSV format for analytics purposes (Columns:  Date, ProductName, Orders)
+CREATE TABLE ProductReporting(
+    ID INT NOT NULL,
+    Date DATE NOT NULL,
+    ProductName VARCHAR(50) NOT NULL,
+    Orders INT NOT NULL
+) ENGINE=CSV;
