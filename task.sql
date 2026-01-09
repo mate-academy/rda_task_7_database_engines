d8c0bb7ce150727b97f249ed0d61b6c5022b670e
Oleg <oleggavelya@gmail.com>
2026-01-09 17:16:51 +0200

    del column id in ProductReporting

--- task.sql
+++ task.sql
@@ -37,7 +37,6 @@ CREATE TABLE Logs(
 
 -- Create a table for storing reporting data, which will be send to a separate application in the CSV format for analytics purposes (Columns:  Date, ProductName, Orders)
 CREATE TABLE ProductReporting(
-    ID INT NOT NULL,
     Date DATE NOT NULL,
     ProductName VARCHAR(50) NOT NULL,
     Orders INT NOT NULL
