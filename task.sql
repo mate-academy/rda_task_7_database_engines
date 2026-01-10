5ae5bcac75024a1d30a1ad09e70e4afadd2f1be0
Andrii <a17drigs@gmail.com>
2026-01-10 20:02:41 +0200

    Solution after AI comment

--- task.sql
+++ task.sql
@@ -14,17 +14,15 @@ CREATE TABLE GeoIPCache (
     IPRange VARCHAR(50),
     CountryID INT,
     PRIMARY KEY (ID),
-    FOREIGN KEY (CountryID) REFERENCES Countries(ID)
 ) ENGINE=Memory;
 
 -- Create a table for storing product descriptions for different countries (Columns: ID, CountryID, ProductID, Description )
-CREATE TABLE ProductDescriptions (
+CREATE TABLE ProductDescription (
     ID INT AUTO_INCREMENT,
     Description TEXT,
     CountryID INT,
     ProductID INT,
     PRIMARY KEY (ID),
-    FOREIGN KEY (CountryID) REFERENCES Countries(ID)
 ) ENGINE=MyISAM;
 -- Create a table for storing logs. For now we don't need to save them, but we need to implement functionality (Columns: ID, Time, LogRecord)
 CREATE TABLE Logs (
@@ -34,7 +32,7 @@ CREATE TABLE Logs (
     PRIMARY KEY (ID)
 ) ENGINE=Blackhole;
 -- Create a table for storing reporting data, which will be send to a separate application in the CSV format for analytics purposes (Columns:  Date, ProductName, Orders)
-CREATE TABLE ReportingData (
+CREATE TABLE ProductReporting (
     Date DATE,
     ProductName VARCHAR(50),
     Orders INT
