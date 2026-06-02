fa5746c41e8c7abd7a46ad0b176071144956d474
Oleksandr Kirichenko <shvdw.on@gmail.com>
2026-06-02 21:03:49 +0300

    Using engines

--- task.sql
+++ task.sql
@@ -1,17 +1,33 @@
-CREATE DATABASE ShopDB; 
-USE ShopDB; 
+CREATE DATABASE ShopDB;
+USE ShopDB;
 
--- Create a table to store countries 
 CREATE TABLE Countries (
     ID INT,
     Name VARCHAR(50),
     PRIMARY KEY (ID)
 ) ENGINE=InnoDB;
 
--- Create a table for caching GeoIP data (Columns: ID, IP Range, CountryID)
+CREATE TABLE GeoIPCache (
+    ID INT,
+    IPRange VARCHAR(50),
+    CountryID INT
+) ENGINE=MEMORY;
 
--- Create a table for storing product descriptions for different countries (Columns: ID, CountryID, ProductID, Description )
+CREATE TABLE ProductDescription (
+    ID INT,
+    Description TEXT,
+    ProductID INT,
+    CountryID INT
+) ENGINE=MyISAM;
 
--- Create a table for storing logs. For now we don't need to save them, but we need to implement functionality (Columns: ID, Time, LogRecord)
+CREATE TABLE Logs (
+    ID INT,
+    Timestamp DATETIME,
+    Message TEXT
+) ENGINE=BLACKHOLE;
 
--- Create a table for storing reporting data, which will be send to a separate application in the CSV format for analytics purposes (Columns:  Date, ProductName, Orders)
+CREATE TABLE ProductReporting (
+    Date DATE,
+    ProductName VARCHAR(100),
+    Orders INT
+) ENGINE=CSV;
