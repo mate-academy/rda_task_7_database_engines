ffe7d91f09905f5d31f5c37ca398c8ab860d9487
Klemjl <juliaklemenchuck06@gmail.com>
2026-06-24 11:45:52 +0300

    Solution

--- task.sql
+++ task.sql
@@ -1,14 +1,13 @@
 CREATE DATABASE ShopDB; 
 USE ShopDB; 
 
--- Create a table to store countries 
 CREATE TABLE Countries (
     ID INT,
     Name VARCHAR(50),
     PRIMARY KEY (ID)
 ) ENGINE=InnoDB;
 
-CREATE TABLE GeopIPCache (
+CREATE TABLE GeoIPCache (
     ID INT,
     IPRange INT,
     CountryID INT,
@@ -26,14 +25,12 @@ CREATE TABLE ProductDescription (
 CREATE TABLE Logs (
     ID INT,
     Timestamp TIME,
-    LogRecord VARCHAR(100),
+    Message VARCHAR(100),
     PRIMARY KEY(ID)
 ) ENGINE=BLACKHOLE;
 
 CREATE TABLE ProductReporting (
-    ID INT,
     Date DATE NOT NULL,
     ProductName VARCHAR(50) NOT NULL,
-    Orders INT NOT NULL,
-    PRIMARY KEY(ID)
-) ENGINE=CSV
+    Orders INT NOT NULL
+) ENGINE=CSV;
