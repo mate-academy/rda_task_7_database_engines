82dbb225f80febcbc7178110f7f897d9b67437af
1ntact <verbaivan9@gmail.com>
2026-06-24 21:45:43 +0200

    DONE!!

--- task.sql
+++ task.sql
@@ -31,7 +31,7 @@ CREATE TABLE Logs (
 ) ENGINE=BLACKHOLE;
 
 CREATE TABLE ProductReporting (
-    Date DATE,
-    ProductName VARCHAR(20),
-    Orders INT
+    Date DATE NOT NULL,
+    ProductName VARCHAR(20) NOT NULL,
+    Orders INT NOT NULL
 ) ENGINE=CSV;
