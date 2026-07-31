f1a32abe533c07b6f40601abed4d2780f723ff45
Vadym23-11 <vadym.rurka01@gmail.com>
2026-07-31 17:52:44 +0200

    new solution

--- task.sql
+++ task.sql
@@ -22,7 +22,7 @@ CREATE TABLE ProductDescription (
     ProductID INT,
     CountryID INT,
     PRIMARY KEY (ID)
-) ENGINE=MyISAM;
+) ENGINE=InnoDB;
 -- Create a table for storing logs. For now we don't need to save them, but we need to implement functionality (Columns: ID, Time, LogRecord)
 CREATE TABLE Logs (
     ID INT,
@@ -32,7 +32,6 @@ CREATE TABLE Logs (
 ) ENGINE=BLACKHOLE;
 -- Create a table for storing reporting data, which will be send to a separate application in the CSV format for analytics purposes (Columns:  Date, ProductName, Orders)
 CREATE TABLE ProductReporting (
-    ID INT NOT NULL,
     Date DATE NOT NULL,
     ProductName VARCHAR(50) NOT NULL,
     Orders INT NOT NULL
