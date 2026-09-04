e66646feafaef88331bab6db8796abec86f584f7
Mykola Hakh <kolyagakh@gmail.com>
2026-09-04 13:11:42 +0300

    Lesson complete

--- task.sql
+++ task.sql
@@ -1,43 +1,40 @@
-CREATE DATABASE ShopDB;
-
-USE ShopDB;
-
--- Create a table to store countries
+CREATE DATABASE ShopDB; 
+USE ShopDB; 
+ 
 CREATE TABLE Countries (
     ID INT,
     Name VARCHAR(50),
     PRIMARY KEY (ID)
 ) ENGINE=InnoDB;
+ 
 
--- Create a table for caching GeoIP data
 CREATE TABLE GeoIPCache (
     ID INT,
-    IP INT,
-    IPRange INT,
+    IPRange VARCHAR(50),
     CountryID INT,
     PRIMARY KEY (ID)
 ) ENGINE=MEMORY;
+ 
 
--- Create a table for storing product descriptions for different countries
 CREATE TABLE ProductDescription (
     ID INT,
-    CountryID INT,
+    Description TEXT,
     ProductID INT,
-    Description VARCHAR(50),
+    CountryID INT,
     PRIMARY KEY (ID)
 ) ENGINE=MyISAM;
+ 
 
--- Create a table for storing logs
 CREATE TABLE Logs (
     ID INT,
     Timestamp DATETIME,
-    Message VARCHAR(100),
+    Message VARCHAR(255),
     PRIMARY KEY (ID)
-) ENGINE=CSV;
+) ENGINE=BLACKHOLE;
+ 
 
--- Create a table for storing reporting data
 CREATE TABLE ProductReporting (
-    ReportDate DATE NOT NULL,
-    ProductName VARCHAR(50) NOT NULL,
+    Date DATE NOT NULL,
+    ProductName VARCHAR(100) NOT NULL,
     Orders INT NOT NULL
-) ENGINE=FEDERATED;
+) ENGINE=CSV;
