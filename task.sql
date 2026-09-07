577f055724cf43811570d033e8589c9fdcbffe4b
Daniil <shpak.daniil06@gmail.com>
2026-09-07 21:25:03 +0300

    Add storage engines

--- task.sql
+++ task.sql
@@ -9,9 +9,33 @@ CREATE TABLE Countries (
 ) ENGINE=InnoDB;
 
 -- Create a table for caching GeoIP data (Columns: ID, IP Range, CountryID)
+CREATE TABLE GeoIPCache(
+	ID INT AUTO_INCREMENT,
+	IPRange VARCHAR(50),
+    CountryID INT,
+	PRIMARY KEY (ID)
+) ENGINE=MEMORY;
 
 -- Create a table for storing product descriptions for different countries (Columns: ID, CountryID, ProductID, Description )
+CREATE TABLE ProductDescription(
+	ID INT AUTO_INCREMENT,
+	CountryID INT,
+	ProductID INT,
+    Description VARCHAR(50),
+	PRIMARY KEY (ID)
+) ENGINE=InnoDB;
 
 -- Create a table for storing logs. For now we don't need to save them, but we need to implement functionality (Columns: ID, Time, LogRecord)
+CREATE TABLE Logs(
+	ID INT AUTO_INCREMENT,
+	Time TIMESTAMP,
+	LogRecord TEXT,
+	PRIMARY KEY (ID)
+) ENGINE=BLACKHOLE;
 
 -- Create a table for storing reporting data, which will be send to a separate application in the CSV format for analytics purposes (Columns:  Date, ProductName, Orders)
+CREATE TABLE Reporting(
+	Date DATE,
+	ProductName VARCHAR(50),
+	Orders INT
+) ENGINE=CSV;
