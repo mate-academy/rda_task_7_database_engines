c2b843240da32bf8b8dc19e981c20d290e7b20ae
Daniil <shpak.daniil06@gmail.com>
2026-09-07 21:27:50 +0300

    Fix CSV table

--- task.sql
+++ task.sql
@@ -35,7 +35,7 @@ CREATE TABLE Logs(
 
 -- Create a table for storing reporting data, which will be send to a separate application in the CSV format for analytics purposes (Columns:  Date, ProductName, Orders)
 CREATE TABLE Reporting(
-	Date DATE,
-	ProductName VARCHAR(50),
-	Orders INT
+	Date DATE NOT NULL,
+	ProductName VARCHAR(50) NOT NULL,
+	Orders INT NOT NULL
 ) ENGINE=CSV;
